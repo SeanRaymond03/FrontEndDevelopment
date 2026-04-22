f39eb2fa7247b5fa9b5373bbf57260f93199f32e
SeanRaymond03 <seanraymond333@gmail.com>
2026-04-22 21:08:55 +0100

    Initial PWA setup - Ionic 7, Angular standalone, HttpClient, Storage, Capacitor

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
+import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { routes } from './app/app.routes';
+import { AppComponent } from './app/app.component';
+import { isDevMode, importProvidersFrom } from '@angular/core';
+import { provideServiceWorker } from '@angular/service-worker';
+import { provideHttpClient } from '@angular/common/http';
+import { IonicStorageModule } from '@ionic/storage-angular';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideIonicAngular(),
+    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideHttpClient(),
+    importProvidersFrom(IonicStorageModule.forRoot()),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
+  ],
+});
